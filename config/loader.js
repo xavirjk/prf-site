2ec101b3a26c83ff9d1a54cd87a23d55df683ee1
@j_kahuha <johntexavir@gmail.com>
2021-07-22 16:27:28 +0300

    ui  links config

--- config/loader.js
+++ config/loader.js
@@ -1,5 +1,6 @@
 const auth = require('./auth');
 const ct = require('./client');
+const ui = require('./ui');
 const { client } = require('./conf_client');
 
 module.exports = (app) => {
@@ -7,4 +8,5 @@ module.exports = (app) => {
   app.use('/update', client);
   app.use('/user', client);
   app.use('/ref', ct);
+  app.use('/p-folio', ui);
 };
